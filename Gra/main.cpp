2993c3ff23b9516f007b6ad76aca27b81d8307f3
JanJoz <jan.jozwiak@student.put.poznan.pl>
2021-05-27 19:02:48 +0200

    update 1

--- Gra/main.cpp
+++ Gra/main.cpp
@@ -2,7 +2,7 @@
 
 
 int main() {
-    // create the window
+    
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
 
@@ -14,17 +14,11 @@ int main() {
 
         sf::Event event;
         while (window.pollEvent(event)) {
-            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        // clear the window with black color
         window.clear(sf::Color::Black);
-
-
-
-        // end the current frame
         window.display();
 
     }
